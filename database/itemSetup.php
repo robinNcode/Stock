95d41fe6d5480788ddd49a1f74b5d8a5a0b3f278
Sajib Adhikary <60180521+Shiftgithub@users.noreply.github.com>
2020-03-17 22:15:51 +0600

    Item Setup Can't insert on db

---
+++ database/itemSetup.php
@@ -0,0 +1,44 @@
+<?php
+ 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sms";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+    $update = false;
+    $id = 0;
+  
+ if (isset($_POST['save']))
+ {
+	$category=$_POST['category'];
+	$company=$_POST['company'];
+ 	$item = $_POST['item'];
+ 	$level=$_POST['level'];
+
+
+ 	$conn->query("INSERT INTO item(item,company,category,level) VALUES('$item','$company','$category', '$level')")or die("Connection failed: " . $conn->connect_error);
+ 	echo "hello";
+ 	$_SESSION ['messege'] = "Information has been Saved!";
+ 	$_SESSION ['msg_type'] = "success";
+ 	header("location: ../index.php?submit=item_setup.php");
+
+ }
+ else if (isset($_GET['delete'])){
+
+ 	$id = $_GET['delete'];
+ 	
+ 	$conn->query("DELETE FROM item WHERE id=$id")or die("Deletion failed: " .$conn->connect_error);
+
+ 	$_SESSION ['messege'] = "Information has been removed!";
+ 	$_SESSION ['msg_type'] = "danger";
+ 	header("location: ../index.php?submit=item_setup.php");
+ }
+
+ ?>
